656b581c06cb5193403a8623159849224cf24091
JUVIS\pytho <python03@naver.com>
2026-04-30 00:49:00 +0900

    feat: 광산 생성 및, 광물 생성, 아웃라인 추가

---
+++ Assets/01.Scripts/Object/Mine.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mine : MonoBehaviour
+{
+    [Header("Mine Data")]
+    [Tooltip("Ore를 스폰할 위치입니다.")][SerializeField] Transform parent;
+    [Tooltip("소환할 Ore 프리팹입니다.")][SerializeField] Ore orePrefab;
+    [Tooltip("가로 세로 몇개씩 소환할지 나타내는 벡터입니다.")][SerializeField] Vector2 array;
+
+
+    [SerializeField] List<Ore> oreList = new List<Ore>();
+
+    bool isInit = false; // 초기화 체크
+    void Start()
+    {
+        if (!isInit) Init();
+    }
+
+    void Init()
+    {
+        for (int i = 0; i < array.x * array.y; i++)
+        {
+            Ore ore = Instantiate(orePrefab, parent);
+            oreList.Add(ore);
+        }
+
+        // 위치 지정
+        for (int y = 0; y < array.y; y++)
+        {
+            for (int x = 0; x < array.x; x++)
+            {
+                Vector3 position = new Vector3(x, 0.5f, y);
+                oreList[x + (int)(array.x * y)].gameObject.transform.localPosition = position;
+            }
+        }
+    }
+}
